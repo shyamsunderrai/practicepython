07f921250e63d00bef163061ef4e5d3b95746c08
shyamsunderrai <shyamsunderrai@gmail.com>
2018-02-19 21:25:12 -0800

    Saving applogparser

---
+++ .idea/bubblesort.py
@@ -0,0 +1,19 @@
+'''
+Bubblesort program
+
+1. Take a list of non-sequential numbers
+   a = [ 0, 5, 3, 7, 8, 11, 20, 12 ]
+
+2. Create a new list with sorted/arranged list
+   out = [ 0, 3, 5, 7, 8, 11, 12, 20 ]
+
+'''
+
+xy = [50, 2, 34, 6, 4, 3, 1, 5, 2]
+xy.sort()
+xy = list(set(xy))
+print xy
+
+
+
+
